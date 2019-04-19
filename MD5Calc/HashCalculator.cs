689028763788381b6b56121fd97cf4dc7934dac3
Leon <leon.id@qq.com>
2019-04-19 11:21:07 +0800

    Hash计算器不使用Lazy+lock，同步性能略微下降，并发性能略微提升；

--- MD5Calc/HashCalculator.cs
+++ MD5Calc/HashCalculator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace MD5Calc
@@ -10,22 +9,13 @@ namespace MD5Calc
     public static class HashCalculator
     {
         /// <summary>
-        /// 哈希算法
-        /// </summary>
-        /// <remarks>线程安全、懒加载</remarks>
-        private static readonly Lazy<HashAlgorithm> hashAlgorithm = new Lazy<HashAlgorithm>(() => new MD5CryptoServiceProvider(), true);
-
-        /// <summary>
         /// 计算文件哈希
         /// </summary>
         /// <param name="plaintText"></param>
         /// <returns></returns>
         public static string GetHash(string plaintText)
         {
-            lock (hashAlgorithm)
-            {
-                return GetString(hashAlgorithm?.Value.ComputeHash(Encoding.UTF8.GetBytes(plaintText)));
-            }
+            return GetString(new MD5CryptoServiceProvider().ComputeHash(Encoding.UTF8.GetBytes(plaintText)));
         }
 
         /// <summary>
